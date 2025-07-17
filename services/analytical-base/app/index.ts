2b5a2704194e51c0d0c2fa86084c09a63878f628
Guillaume Royer <groy@fiveonefour.com>
2025-07-17 18:57:01 -0400

    moved the event pipeline creation to the eventsPipiline file

--- services/analytical-base/app/index.ts
+++ services/analytical-base/app/index.ts
@@ -24,31 +24,8 @@
 // Need help? Check out the quickstart guide:
 // → docs.fiveonefour.com/moose/getting-started/quickstart
 
-import { FooWithCDC, BarWithCDC } from "@workspace/models";
-import { IngestPipeline } from "@514labs/moose-lib";
-
-export const FooPipeline = new IngestPipeline<FooWithCDC>("Foo", {
-  table: {
-    orderByFields: ["cdc_id", "cdc_timestamp"],
-    deduplicate: true,
-  },
-  stream: true,
-  ingest: true,
-});
-
-
-export const BarPipeline = new IngestPipeline<BarWithCDC>("Bar", {
-  table: {
-    orderByFields: ["cdc_id", "cdc_timestamp"],
-    deduplicate: true,
-  },
-  stream: true,
-  ingest: true,
-});
-
-
 export * from "./pipelines/eventsPipeline";
- export * from "./views/foo";
+export * from "./views/foo";
 export * from "./apis/FooConsumptionApi";
 export * from "./apis/BarConsumptionApi";
 export * from "./apis/FooCurrentStateApi";
