66b71fc0b4220f849264f39bcfd063a7d5de8ed7
Guillaume Royer <groy@fiveonefour.com>
2025-08-05 10:23:53 -0400

    removed unused endpoint.

--- ufa/services/analytical-moose-foobar/app/apis/foo/consumption/foo-average-score.ts
+++ ufa/services/analytical-moose-foobar/app/apis/foo/consumption/foo-average-score.ts
@@ -2,11 +2,9 @@ import { ConsumptionApi } from "@514labs/moose-lib";
 import { FooPipeline } from "../../../index";
 import { GetFoosAverageScoreResponse } from "@workspace/models/foo";
 
-// Type for endpoints with no parameters
 // eslint-disable-next-line
 type EmptyParams = {};
 
-// New endpoint to calculate average score
 export const fooAverageScoreApi = new ConsumptionApi<
   EmptyParams,
   GetFoosAverageScoreResponse
@@ -44,45 +42,3 @@ export const fooAverageScoreApi = new ConsumptionApi<
     };
   }
 );
-
-// New endpoint to get CDC operation statistics
-export const fooCdcStatsApi = new ConsumptionApi<
-  EmptyParams,
-  { operationCounts: Record<string, number>; queryTime: number }
->("foo-cdc-stats", async (_params: EmptyParams, { client, sql }) => {
-  const startTime = Date.now();
-
-  const query = sql`
-      SELECT 
-        cdc_operation,
-        COUNT(*) as count
-      FROM ${FooPipeline.table!}
-      WHERE cdc_operation IS NOT NULL
-      GROUP BY cdc_operation
-    `;
-
-  const resultSet = await client.query.execute<{
-    cdc_operation: string;
-    count: number;
-  }>(query);
-
-  const results = (await resultSet.json()) as {
-    cdc_operation: string;
-    count: number;
-  }[];
-
-  const operationCounts = results.reduce(
-    (acc, row) => {
-      acc[row.cdc_operation] = row.count;
-      return acc;
-    },
-    {} as Record<string, number>
-  );
-
-  const queryTime = Date.now() - startTime;
-
-  return {
-    operationCounts,
-    queryTime,
-  };
-});
